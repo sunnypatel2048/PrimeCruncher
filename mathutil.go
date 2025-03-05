46738e5c87fe2c01e0d79e8061ed0f6e7cbc453e
Sunny Patel <sunnypatel2048@gmail.com>
2025-03-04 20:34:11 -0500

    updated isPrime logic

--- mathutil.go
+++ mathutil.go
@@ -5,5 +5,5 @@ import "math/big"
 // isPrime returns true if n is a prime number, and false otherwise
 func isPrime(n uint64) bool {
 	bigInt := new(big.Int).SetUint64(n)
-	return bigInt.ProbablyPrime(0)
+	return bigInt.ProbablyPrime(5)
 }
